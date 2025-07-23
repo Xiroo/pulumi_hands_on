72f4e6ce670154d3f5063f2df78cf76a27f09e07
kyeonghoon Kim <khkh1188@gmail.com>
2025-07-23 14:53:47 +0900

    chore: add comment

--- step4.py
+++ step4.py
@@ -1,6 +1,8 @@
 import pulumi
 import pulumi_kubernetes as k8s
 
+# Befor doing 'pulumi up', you need to apply step4_configmap.yaml to make collision
+# pulumi import kubernetes:core/v1:ConfigMap step4_collision default/collision
 class Step4(pulumi.ComponentResource):
     def __init__(self, name: str, opts: pulumi.ResourceOptions = None):
         super().__init__("custom:resource:Step4", name, {}, opts)
